5b994aff0bb7298875d2e7443044e72fc39cdb4f
David Fudacz <davidfudacz@gmail.com>
2018-02-16 21:48:29 -0600

    Initial commit

---
+++ functions.js
@@ -0,0 +1,20 @@
+function Contact (firstName, lastName, email, phone) {
+  this.firstName = firstName;
+  this.lastName = lastName; 
+  this.email = {'main': email};
+  this.phone = {'main': phone};
+
+}
+
+Contact.prototype.addAddress = function(street,street2,city,state,zip) {
+  this.address = {
+    'street': street,
+    'street2': street2,
+    'city': city,
+    'state': state,
+    'zip': zip,
+};
+}
+
+Contact.prototype.printAddress = function () {
+  console.log(this.address.street + "\n" + if (this.address.street2) {this.address.street2 + "\n" + } this.address.city + ', ' + this.address.state + ' ' + this.address.zip);
