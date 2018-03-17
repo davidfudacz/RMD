4732c1d0a1cdfd5a0625e9130d5832ef9f647a26
David Fudacz <davidfudacz@gmail.com>
2018-03-17 14:25:53 -0500

    functionality for relationship add page

--- routes/relationships.js
+++ routes/relationships.js
@@ -32,7 +32,6 @@ router.get('/add/:id', (req,res,next) => {
 
   Promise.all([contact,everyoneElse,relationshipTypes])
     .then(([contact,everyoneElse,relTypes]) => {
-      console.log(relTypes);
       res.render('addRelationships', {
         contact: contact,
         everyoneElse: everyoneElse,
@@ -45,29 +44,65 @@ router.get('/add/:id', (req,res,next) => {
 
 router.post('/add/:id', (req,res,next) => {
   const contact = Contact.findById(req.params.id);
-  const everyoneElse = Contact.findAll({
-    where: {
-      id: {
-        $$ne: req.params.id
-      }
-    },
-    order: [
-      ['lastName', 'ASC'],
-      ['firstName', 'ASC'],
-    ],
-  })
-  const relationshipTypes = RelationshipType.findAll();
+  const target = Contact.findById(req.body.contact);
+  const relationshipType = RelationshipType.findById(req.body.relationship);
 
-  Promise.all([contact,everyoneElse,relationshipTypes])
-    .then(([contact,everyoneElse,relTypes]) => {
-      console.log(relTypes);
-      res.render('addRelationships', {
-        contact: contact,
-        everyoneElse: everyoneElse,
-        relTypes: relTypes,
-      });
+  Promise.all([contact,target,relationshipType])
+    .then(([contact,target,relType]) => {
+      console.log(relType.singular);
+      switch (relType.singular) {
+        case 'Child': {
+          Promise.all([
+            contact.addChild(target),
+            target.addParent(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+        case 'Parent': {
+          Promise.all([
+            contact.addParent(target),
+            target.addChild(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+        case 'Friend': {
+          Promise.all([
+            contact.addFriend(target),
+            target.addFriend(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+        case 'Other': {
+          Promise.all([
+            contact.addOther(target),
+            target.addOther(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+        case 'Relative': {
+          Promise.all([
+            contact.addRelative(target),
+            target.addRelative(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+        case 'Sibling': {
+          Promise.all([
+            contact.addSibling(target),
+            target.addSibling(contact)
+          ])
+          .catch(console.error.bind(console));
+        };
+          break;
+      }
     })
-    .catch(next);
+  res.json(req.body);
+  
 })
 
 module.exports = router;
